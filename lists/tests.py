1810b7c637d7c0e525379a2be7fd40f0c84b0a4d
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2017-02-19 16:16:51 +0000

    Home can now handle 1 list element - Commit before refactoring functional tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,12 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.test import SimpleTestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-class HomePageTest(TestCase):
+class HomePageTest(SimpleTestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -14,5 +15,26 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertTemplateUsed('home.html')
+
+# Book method for testing html was the same didn´t work with crsf token
+#        expected_html = render_to_string('home.html', request=request)
+#        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertContains(response,'A new list item')
+
+# Book method (below) didn't work with csrf token
+#        self.assertIn('A new list item', response.content.decode())
+#        expected_html = render_to_string(
+#            'home.html',
+#            {'new_item_text': 'A new list item'}
+#        )
+#        self.assertEqual(response.content.decode(), expected_html)
+
