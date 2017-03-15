081fa162ae20b8fd4d463982da1c6397f18f44c8
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2017-03-15 23:28:10 +0000

    Replace explicit waits in FTs for a helper function specific for rows in the list

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -26,44 +26,40 @@ class ItemValidationTest(FunctionalTest):
         # She tries again with some text for the item, which now works
         inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk'+Keys.ENTER)
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
         inputbox = self.get_item_input_box()
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(2)
-
         # She receives a similar warning on the list page
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
         error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filing some text in
         inputbox = self.get_item_input_box()
         inputbox.send_keys('Make tea'+Keys.ENTER)
-        time.sleep(2)
         
-        self.check_for_row_in_list_table('1: Buy milk')
-        self.check_for_row_in_list_table('2: Make tea')
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
     def test_cannot_add_duplicate_items(self):
         # Edith goes to the home page and starts a new list
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('Buy wellies'+Keys.ENTER)
-        time.sleep(2)
         
-        self.check_for_row_in_list_table('1: Buy wellies')
+        self.wait_for_row_in_list_table('1: Buy wellies')
 
         # She accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies'+Keys.ENTER)
-        time.sleep(2)
 
         # She sees a helpful error message
-        self.check_for_row_in_list_table('1: Buy wellies')
+        self.wait_for_row_in_list_table('1: Buy wellies')
         error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
 
+    @skip
     def test_error_messages_are_cleared_on_input(self):
         # Edith starts a new list in a way that causes a validation error
         self.browser.get(self.server_url)
